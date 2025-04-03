2c644583458c08b183e9431c8297dbbef6b41717
NirupamaD99 <150535823+NirupamaD99@users.noreply.github.com>
2025-04-03 12:12:04 +0530

    added System User and roles CRUD operations

---
+++ SFCDashboard/Controllers/SystemUsersController.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SFCDashboard.Data;
+
+namespace SFCDashboard.Controllers
+{
+    public class SystemUsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SystemUsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SystemUsers
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Users.Include(s => s.UserRole).Include(s => s.WorkGroup);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: SystemUsers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var systemUser = await _context.Users
+                .Include(s => s.UserRole)
+                .Include(s => s.WorkGroup)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (systemUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Create
+        public IActionResult Create()
+        {
+            ViewData["UserRoleId"] = new SelectList(_context.UserRole, "Id", "Name");
+            ViewData["WorkGroupId"] = new SelectList(_context.WorkGroups, "Id", "Name");
+            return View();
+        }
+
+        // POST: SystemUsers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ServiceId,UserRoleId,WorkGroupId")] SystemUser systemUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(systemUser);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserRoleId"] = new SelectList(_context.UserRole, "Id", "Name", systemUser.UserRoleId);
+            ViewData["WorkGroupId"] = new SelectList(_context.WorkGroups, "Id", "Name", systemUser.WorkGroupId);
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var systemUser = await _context.Users.FindAsync(id);
+            if (systemUser == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserRoleId"] = new SelectList(_context.UserRole, "Id", "Name", systemUser.UserRoleId);
+            ViewData["WorkGroupId"] = new SelectList(_context.WorkGroups, "Id", "Name", systemUser.WorkGroupId);
+            return View(systemUser);
+        }
+
+        // POST: SystemUsers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ServiceId,UserRoleId,WorkGroupId")] SystemUser systemUser)
+        {
+            if (id != systemUser.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(systemUser);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SystemUserExists(systemUser.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserRoleId"] = new SelectList(_context.UserRole, "Id", "Name", systemUser.UserRoleId);
+            ViewData["WorkGroupId"] = new SelectList(_context.WorkGroups, "Id", "Name", systemUser.WorkGroupId);
+            return View(systemUser);
+        }
+
+        // GET: SystemUsers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var systemUser = await _context.Users
+                .Include(s => s.UserRole)
+                .Include(s => s.WorkGroup)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (systemUser == null)
+            {
+                return NotFound();
+            }
+
+            return View(systemUser);
+        }
+
+        // POST: SystemUsers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var systemUser = await _context.Users.FindAsync(id);
+            if (systemUser != null)
+            {
+                _context.Users.Remove(systemUser);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SystemUserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
+        }
+        [HttpGet]
+        public JsonResult SearchWorkgroups(string term)
+        {
+            if (string.IsNullOrWhiteSpace(term))
+                return Json(new { });
+
+            var workgroups = _context.WorkGroups
+                .Where(w => w.Name.ToLower().Contains(term.ToLower()))
+                .Select(w => new { id = w.Id, label = w.Name })
+                .Take(20)
+                .Distinct()
+                .ToList();
+
+            return Json(workgroups);
+        }
+
+    }
+}
---
+++ SFCDashboard/Migrations/20250402100705_ChangeSystemUser.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SFCDashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSystemUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "ServiceId");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Users",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ServiceId",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Users",
+                newName: "FullName");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
