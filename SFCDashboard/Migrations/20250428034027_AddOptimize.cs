65ca9996a7730c8d19ada66291e04efef69b2b46
Ashini Samaranayake <144650127+AshSama12@users.noreply.github.com>
2025-04-28 11:37:27 +0530

    update tables and frontend

---
+++ SFCDashboard/Migrations/20250428034027_AddOptimize.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SFCDashboard.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOptimize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsUrgent",
+                table: "PETasks");
+
+            migrationBuilder.DropColumn(
+                name: "UrgentRequested",
+                table: "PETasks");
+
+            migrationBuilder.RenameColumn(
+                name: "Priority",
+                table: "PETasks",
+                newName: "TaskPhase");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TaskPhase",
+                table: "PETasks",
+                newName: "Priority");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUrgent",
+                table: "PETasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UrgentRequested",
+                table: "PETasks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
